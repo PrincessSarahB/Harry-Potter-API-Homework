bf19abeffd1804ad5c47196ae20034add87dbc0e
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-16 12:55:43 +0100

    app and make request functions written

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
+const app = function(){
+  const url = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+  makeRequest(url, requestComplete);
+}
 
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+
+}
+
+
+
+window.addEventListener(load, app);
