29c790b718c15a6cb6520bb2a0b80269e5f79f13
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-17 13:49:48 +0100

    background colour and text colour changes when character selected

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 const app = function(){
-  const url = "http://hp-api.herokuapp.com/api/characters/students";
-  makeRequest(url, requestComplete);
+  const urlStudent = "http://hp-api.herokuapp.com/api/characters/students";
+  makeRequest(urlStudent, requestComplete);
 }
 
 const makeRequest = function(url, callback){
@@ -20,8 +20,13 @@ select.addEventListener('change', function(){
   var student = students[select.value];
   handleSelectChange(student)
 });
+  addImage();
 };
 
+const addImage = function(){
+  crest = document.querySelector('#crest');
+  crest.src = "https://vignette.wikia.nocookie.net/harrypotter/images/6/6d/Hogwarts_crest.jpg/revision/latest?cb=20080209002605"
+}
 
 const populateDropdown = function(students){
   const dropdown = document.querySelector('#students');
@@ -36,6 +41,8 @@ const populateDropdown = function(students){
 
 const handleSelectChange = function(student){
   const ul = document.querySelector('#selected-student');
+  const body = document.getElementsByTagName('body')[0];
+  body.setAttribute("class", student.house);
   const img = document.querySelector('#img')
   img.src = student.image;
   img.height = 400;
@@ -44,7 +51,11 @@ const handleSelectChange = function(student){
   const houseLi = document.querySelector('#houseLi');
   houseLi.textContent = "House: " + student.house;
   const ancestryLi = document.querySelector('#ancestryLi');
-  ancestryLi.textContent = "Ancestry: " + student.ancestry;
+  if(student.ancestry !== ""){
+  ancestryLi.textContent = "Ancestry: " + student.ancestry;}
+  else {
+   ancestryLi.textContent = "Ancestry: Unknown";
+  }
   const patronusLi = document.querySelector('#patronusLi');
   if(student.patronus !== ""){
   patronusLi.textContent = "Patronus: " + student.patronus;
